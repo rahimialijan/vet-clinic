3a98080fa81831b8f5e153d117942bf7ae62a346
Alijan Rahimi <alijanrahimi24@yahoo.com>
2023-07-17 20:11:42 +0500

    Create db and animals table

--- schema.sql
+++ schema.sql
@@ -1,5 +1,13 @@
-/* Database schema to keep the structure of entire database. */
+/* Database schema to keep the structure of the entire database. */
 
-CREATE TABLE animals (
-    name varchar(100)
-);
+-- Create the vet_clinic database
+CREATE DATABASE vet_clinic;
+
+-- Create the animals' table
+ CREATE TABLE animals(
+ id BIGSERIAL NOT NULL PRIMARY KEY,
+ name VARCHAR(50) NOT NULL,
+date_of_birth DATE NOT NULL,
+escape_attempts SERIAL NOT NULL,
+neutered BOOLEAN NOT NULL,
+ weight_kg DECIMAL(10, 2) NOT NULL);
