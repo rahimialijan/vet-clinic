d54d337e0553b8c7ea5ce952c887b98c5a472ccf
Alijan Rahimi <alijanrahimi24@yahoo.com>
2023-07-18 20:54:24 +0500

    Modify the weight_kg values

--- data.sql
+++ data.sql
@@ -8,9 +8,11 @@ VALUES ('Agumon', '2020-02-03', 0, true, 10.23),
        ('Charmander', '2020-02-08', 0, false, 11),
 ----------------------------insert new values for animals table----------------------------------------
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
-VALUES ('Plantmon', '2021-11-15', 2, true, 5.7),
-        ('Squirtle', '1993-04-02', 3, false, 12.13),
-        ('Angemon', '2005-06-12', 1, true, 45),
-        ('Boarmon', '2005-06-07', 7, true, 20.4),
-        ('Blossom', '1998-10-13', 3, true, 17),
-        ('Ditto', '2022-05-14', 4, true, 22);
+VALUES 
+  ('Charmander', '2020-02-08', 0, FALSE, -11),
+  ('Plantmon', '2021-11-15', 2, TRUE, -5.7),
+  ('Squirtle', '1993-04-02', 3, FALSE, -12.13),
+  ('Angemon', '2005-06-12', 1, TRUE, -45),
+  ('Boarmon', '2005-06-07', 7, TRUE, 20.4),
+  ('Blossom', '1998-10-13', 3, TRUE, 17),
+  ('Ditto', '2022-05-14', 4, TRUE, 22);
