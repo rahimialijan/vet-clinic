4ec80d6c41acffc7bcb25e00a056a8660926d682
Alijan Rahimi <alijanrahimi24@yahoo.com>
2023-07-20 18:11:48 +0500

    Create vets, specializations and visits table and insert values for all 3 tables

--- schema.sql
+++ schema.sql
@@ -42,3 +42,26 @@ ALTER TABLE
  ALTER TABLE animals
  ADD COLUMN owner_id INTEGER REFERENCES owners (id);
 ALTER TABLE
+
+--- create vet table
+CREATE TABLE vets (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(100),
+  age INTEGER,
+  date_of_graduation DATE
+);
+
+-- Create the specializations join table
+CREATE TABLE specializations (
+  vet_id INTEGER REFERENCES vets(id),
+  species_id INTEGER REFERENCES species(id),
+  PRIMARY KEY (vet_id, species_id)
+);
+
+-- Create the visits join table
+CREATE TABLE visits (
+  animal_id INTEGER REFERENCES animals(id),
+  vet_id INTEGER REFERENCES vets(id),
+  visit_date DATE,
+  PRIMARY KEY (animal_id, vet_id, visit_date)
+);
